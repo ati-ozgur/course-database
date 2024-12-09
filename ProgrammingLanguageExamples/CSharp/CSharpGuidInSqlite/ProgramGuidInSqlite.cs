cc49bee7f15eaf3a9b538d3e46e63692d4041348
Atilla Ozgur <ati.ozgur@gmail.com>
2024-12-09 18:00:09 +0300

    Programming language example is added

---
+++ ProgrammingLanguageExamples/CSharp/CSharpGuidInSqlite/ProgramGuidInSqlite.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Microsoft.Data.Sqlite;
+
+namespace GuidInSqlite
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            using (var connection = new SqliteConnection("Data Source=:memory:"))
+            {
+                connection.CreateFunction("newid", Guid.NewGuid); 
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText =
+                @"
+                    SELECT newid()
+                ";
+                var newId = command.ExecuteScalar();
+
+                Console.WriteLine($"Your GUID is {newId}.");
+            }
+        }
+    }
+}
